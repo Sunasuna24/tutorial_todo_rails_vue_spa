e705e1574b0e756a596f22c09fd5e74a51011a56
Tomoyasu Sunagawa <tomoyasusunagawa@mac408-sunagawa.local>
2024-05-28 14:10:29 +0900

    Postモデルの正常系のテストを書いた

---
+++ spec/models/post_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe Post, type: :model do
+  let(:user) { FactoryBot.create(:user) }
+
+  it "is valid with valid attributes" do
+    post_info = {
+      title: "My Title",
+      body: "This is a body of a test post.",
+      user_id: user.id
+    }
+    post = Post.create(post_info)
+
+    expect(post).to be_valid
+  end
+end
