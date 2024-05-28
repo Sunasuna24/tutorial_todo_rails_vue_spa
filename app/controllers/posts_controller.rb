262d6bafdab8dfb622bdcbd454e87f8cefa1ae65
Tomoyasu Sunagawa <tomoyasusunagawa@mac408-sunagawa.local>
2024-05-28 12:00:35 +0900

    投稿を作成する機能を追加した

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,17 @@
 class PostsController < ApplicationController
   def create
-    message = "Hello, " + params[:name] + "!"
-    sample = {message: message}
-    render json: sample
+    user_id = 1 # 後に動的にしたい
+    title = params[:title]
+    body = params[:body]
+
+    post_data = {
+      title: title,
+      body: body,
+      user_id: user_id
+    }
+
+    post = Post.create(post_data)
+
+    render json: post
   end
 end
