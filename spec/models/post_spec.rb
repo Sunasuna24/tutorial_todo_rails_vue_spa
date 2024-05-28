d76462dc2231bf95241d0a104c3b756566b9a90b
Tomoyasu Sunagawa <tomoyasusunagawa@mac408-sunagawa.local>
2024-05-28 14:33:18 +0900

    FactoryBotの記載を省略した

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,20 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  let(:user) { FactoryBot.create(:user) }
+  let(:user) { create(:user) }
 
   it "is valid with valid attributes" do
-    post_info = {
-      title: "My Title",
-      body: "This is a body of a test post.",
-      user_id: user.id
-    }
-    post = Post.create(post_info)
-
+    post = create(:post, user: user)
+    
     expect(post).to be_valid
   end
-
+  
   it "is not valid when there is no title" do
+    # post = FactoryBot.create(:post, title: nil, user: user)
     post_info = {
       body: "This is a body of a test post.",
       user_id: user.id
@@ -22,6 +18,7 @@ RSpec.describe Post, type: :model do
     post = Post.create(post_info)
 
     expect(post).to be_invalid
+    # expect(FactoryBot.create(:post, title: nil, user: user)).to be_invalid
   end
 
   it "is not valid when there title is empty" do
