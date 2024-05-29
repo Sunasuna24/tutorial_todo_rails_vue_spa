e22f6d9576ad26fe6f9a380918a823109920b5c1
Tomoyasu Sunagawa <tomoyasusunagawa@mac408-sunagawa.local>
2024-05-29 16:31:29 +0900

    全ての投稿を返すテストを作成した

---
+++ spec/requests/posts_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe "Posts", type: :request do
+
+  describe "GET /posts" do
+    let(:post_number) { 5 }
+    before do
+      create_list(:post, post_number)
+      get '/posts'
+    end
+
+    it "returns 200 status" do
+      expect(response).to have_http_status(200)
+    end
+
+    it "returns all posts" do
+      posts = JSON.parse(response.body)
+
+      expect(posts.size).to eq(post_number)
+    end
+  end
+end
