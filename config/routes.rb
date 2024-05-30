b8a25ee053356680db90633ea211b5ac28a3ecb6
Tomoyasu Sunagawa <tomoyasusunagawa@mac408-sunagawa.local>
2024-05-30 13:05:22 +0900

    投稿を削除する処理を追加した

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get '/test', to: 'posts#test'
   post '/post', to: 'posts#create'
-  get '/posts/:id', to: 'posts#show'
   get '/posts', to: 'posts#all'
+  get '/posts/:id', to: 'posts#show'
+  delete '/posts/:id', to: 'posts#destroy'
 end
